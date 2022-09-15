c4576558d420bca0b866e05ceaee7644582d49f4
CarrotJoe <97610630+CarrotJoe@users.noreply.github.com>
2022-09-15 11:59:50 +0100

    1 file modified

--- navbar.tsx
+++ navbar.tsx
@@ -1,27 +1,36 @@
 import * as React from 'react';
 import './style.css';
 
+function myFunction() {
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'topnav';
+  }
+}
+
 export default function Navbar() {
   return (
-    <div class="topnav" id="myTopnav">
-      <a href="#home" class="active">
+    <div className="topnav" id="myTopnav">
+      <a href="#home" className="active">
         Home
       </a>
       <a href="#news">News</a>
       <a href="#contact">Contact</a>
-      <div class="dropdown">
-        <button class="dropbtn">
+      <div className="dropdown">
+        <button className="dropbtn">
           Dropdown
-          <i class="fa fa-caret-down"></i>
+          <i className="fa fa-caret-down"></i>
         </button>
-        <div class="dropdown-content">
+        <div className="dropdown-content">
           <a href="#">Link 1</a>
           <a href="#">Link 2</a>
           <a href="#">Link 3</a>
         </div>
       </div>
       <a href="#about">About</a>
-      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+      <a href="javascript:void(0);" className="icon" onClick="myFunction()">
         &#9776;
       </a>
     </div>
