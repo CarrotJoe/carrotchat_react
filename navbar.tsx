b71b0994692f51240e2d63ab7d02eb5ec3e3053f
CarrotJoe <97610630+CarrotJoe@users.noreply.github.com>
2022-09-15 11:17:02 +0100

    2 files modified

--- navbar.tsx
+++ navbar.tsx
@@ -2,33 +2,28 @@ import * as React from 'react';
 import './style.css';
 
 export default function Navbar() {
-  return(
-    
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  
-  <nav>
-    <div className="container-fluid">
-    <div className="navbar-header">
-      <a className="navbar-brand" href="#">Carrotchat</a>
+  return (
+    <div class="topnav" id="myTopnav">
+      <a href="#home" class="active">
+        Home
+      </a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <div class="dropdown">
+        <button class="dropbtn">
+          Dropdown
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+      <a href="#about">About</a>
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        &#9776;
+      </a>
     </div>
-    <ul className="nav navbar-nav">
-      <li className="active"><a href="#">Home</a></li>
-      <li className="dropdown"><a className="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span className="caret"></span></a>
-        <ul className="dropdown-menu">
-          <li><a href="#">Page 1-1</a></li>
-          <li><a href="#">Page 1-2</a></li>
-          <li><a href="#">Page 1-3</a></li>
-        </ul>
-      </li>
-      <li><a href="#">Page 2</a></li>
-    </ul>
-    <ul className="nav navbar-nav navbar-right">
-      <li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
-    </ul>
-  </div>
-</nav>
   );
-};
+}
