1f276343c4654e7b5c1a3710b9b0a35da5863951
CarrotJoe <97610630+CarrotJoe@users.noreply.github.com>
2022-09-15 15:29:01 +0100

    1 file modified

--- App.tsx
+++ App.tsx
@@ -20,39 +20,3 @@ const firebaseConfig = {
   measurementId: 'G-PBNB7MSH4H',
 };
 
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-
-const auth = getAuth();
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
-
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-
-import { getAuth, signOut } from 'firebase/auth';
-
-signOut(auth)
-  .then(() => {
-    // Sign-out successful.
-  })
-  .catch((error) => {
-    // An error happened.
-  });
