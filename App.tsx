7d33d80a83f312bdf9d734f58cd579dfa3a76009
CarrotJoe <97610630+CarrotJoe@users.noreply.github.com>
2022-09-15 14:32:10 +0100

    4 files modified

--- App.tsx
+++ App.tsx
@@ -9,3 +9,13 @@ export default function App() {
     </div>
   );
 }
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBScIsi0Me_3rpfqCds-Q7M7wiW9oBIUlg',
+  authDomain: 'carrotchat-d4b3b.firebaseapp.com',
+  projectId: 'carrotchat-d4b3b',
+  storageBucket: 'carrotchat-d4b3b.appspot.com',
+  messagingSenderId: '606350056623',
+  appId: '1:606350056623:web:eb75580b253a74b34b085a',
+  measurementId: 'G-PBNB7MSH4H',
+};
--- index.tsx
+++ index.tsx
@@ -15,3 +15,4 @@ root.render(
 );
 
 root.render(<Navbar />);
+
